4d19e6f953cce16a72020cfd4cdb7647f30d4855
andrei-mihaicanuta <andrei.canuta@metro.digital>
2022-07-18 10:44:13 +0300

    added streams for getByCustomerId

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -10,16 +10,14 @@ import java.util.stream.Collectors;
 
 public class CustomerController {
     CustomerService customerService = new CustomerService();
-    CustomerRepository customerRepository = new CustomerRepository();
 
     public List<Customer> getAllCustomers () {
         return customerService.getAllCustomers();
     }
 
-
     public Customer getByCustomerId (CustomerId customerId) {
         try {
-            List<Customer> customers = customerRepository.getAllCustomers()
+            List<Customer> customers = customerService.getAllCustomers()
                     .stream()
                     .filter(c -> c.getCustomerId().equals(customerId))
                     .collect(Collectors.toList());
