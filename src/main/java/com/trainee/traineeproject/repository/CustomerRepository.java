74c79689c605d535478d17700f5645a8ad463ab7
Valentin Protiuc <valentin.protiuc@metro.digital>
2022-07-26 15:11:25 +0300

    updated CustomerRepository

--- src/main/java/com/trainee/traineeproject/repository/CustomerRepository.java
+++ src/main/java/com/trainee/traineeproject/repository/CustomerRepository.java
@@ -1,22 +1,9 @@
 package com.trainee.traineeproject.repository;
 
-import com.trainee.traineeproject.data.Database;
 import com.trainee.traineeproject.entity.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class CustomerRepository {
-    @Autowired
-    private Database database;
-
-    public List<Customer> getAllCustomers () {
-        return database.getCustomers();
-    }
-
-    public void addCustomer (Customer customer) {
-        database.addCustomer(customer);
-    }
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 }
